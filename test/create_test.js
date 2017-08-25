ffac1371ff40122f5dd111517cadaf9206c57167
Travis <tgrieshiemer@gmail.com>
2017-08-25 18:33:22 +0000

    continuing with project

---
+++ test/create_test.js
@@ -0,0 +1,10 @@
+const assert = require('assert');
+const User = require('../src/user.js');
+
+describe('creating records', () => {
+    it('saves a user', () => {
+        const joe = new User({name: 'Joe'}); 
+        
+        joe.save();
+    }); 
+});
