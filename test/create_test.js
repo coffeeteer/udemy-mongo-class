3ef530d49ac9dd7da749cc6ee5d9aa9698f502d5
Coffeeteer <tgrieshiemer@gmail.com>
2017-08-25 22:41:19 -0400

    completed lecture 27 in the Udemy Mongoose class

--- test/create_test.js
+++ test/create_test.js
@@ -2,11 +2,15 @@ const assert = require('assert');
 const User = require('../src/user.js');
 
 describe('creating records', () => {
-	it('saves a user', ()=>{
+	it('saves a user', (done) => {
 		const joe = new User({name: 'Joe'});
 
-		joe.save();
+		joe.save()
+			.then(() => {
+				//Has Joe been saved successfully?
+				assert(!joe.isNew);
+				done();
+			});
 	});
 });
 
-// beforeEach()
--- test/test_helper.js
+++ test/test_helper.js
@@ -1,9 +1,27 @@
 const mongoose = require('mongoose');
 
-mongoose.connect('mongodb://localhost/user_test', { useMongoClient: true })
+mongoose.Promise = global.Promise; //ES6 Promise
 
-var db = mongoose.connection	
-	.on('error', console.error.bind(console, 'You have a database connection error:'))
-	.once('open', function() {
-		console.log('You have connected to mongoose')
+mongoose.connect('mongodb://localhost/user_test', { useMongoClient: true });
+
+before((done) => {
+	mongoose.connection	
+		.on('error', console.error.bind(console, 'You have a database connection error:'))
+		.once('open', function() {
+			console.log('You have connected to mongoose');
+			done();
+		});
+});
+	
+
+// Drops the last instance before the code runs
+beforeEach((done) => {
+	mongoose.connection.collections.users.drop(() => {
+		// making mocha take a 'pause' before it runs the next test done() does this
+
+		// Ready to run our next test
+		//done() ends this function quickly
+		done();
+		// The order goes beforeEach > Start a long running process > Call 'done' > Tests continue running
 	});
+});	
