7da12e66c031915ee83f978cfa0a085a52293682
Coffeeteer <tgrieshiemer@gmail.com>
2017-08-30 18:17:59 -0400

    finished the 'remove' of the portion of the mongoose lectures, lecture 34

--- test/delete_test.js
+++ test/delete_test.js
@@ -19,11 +19,31 @@ describe('deleting a user', () => {
 			});
 	});
 
-	it('class method remove', () => {
-
+	it('class method remove', (done) => {
+		//Remove a bunch of records with come given criteria
+		User.remove({name: 'Joe'})
+			.then(() => User.findOne({name: 'Joe'}))
+			.then((user) => {
+				assert( user === null);
+				done();
+			})
 	});
 
-	it('class method findByIdAndRemove', () => {
+	it('class method findOneAndRemove', (done) => {
+		User.findOneAndRemove({ name: 'Joe' })
+			.then(() => User.findOne({name: 'Joe'}))
+			.then((user) => {
+				assert(user === null);
+				done();
+			});
+	});
 
+	it('class method findByIdAndRemove', (done) => {
+		User.findByIdAndRemove(joe._id)
+			.then(() => User.findOne({ name: 'Joe' }))
+			.then((user) => {
+				assert(user === null);
+				done();
+			})
 	});
 });
