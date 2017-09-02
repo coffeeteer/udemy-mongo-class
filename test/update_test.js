aa2888f57734d11c8385728bba92e10eab37c3d2
Coffeeteer <tgrieshiemer@gmail.com>
2017-09-02 19:49:10 -0400

    finished lecture 36, with updating an instance using the set n save method

--- test/update_test.js
+++ test/update_test.js
@@ -4,13 +4,21 @@ const User = require('../src/user');
 describe('Updating records', () => {
 	var joe;
 
-	beforeEach(() => {
+	beforeEach((done) => {
 		joe = new User({name: 'Joe'});
 		joe.save()
 			.then(() => done());
 	});
 
-	it('instance type using set n save', () => {
-
-	})
+	it('instance type using set n save', (done) => {
+		// set is instance method that allows you update a property
+		joe.set('name', 'Alex');
+		joe.save()
+			.then(() => User.find({}))
+			.then((users) => {
+				assert(users.length === 1);
+				assert(users[0].name === 'Alex');
+				done();
+			});
+	});
 });
