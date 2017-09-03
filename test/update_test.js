23dbed6394e3bcafdde1083e9d20434e2edefcbe
Coffeeteer <tgrieshiemer@gmail.com>
2017-09-02 20:46:28 -0400

    lecture 37 complete, created a helper function, and learned about update

--- test/update_test.js
+++ test/update_test.js
@@ -10,15 +10,25 @@ describe('Updating records', () => {
 			.then(() => done());
 	});
 
-	it('instance type using set n save', (done) => {
-		// set is instance method that allows you update a property
-		joe.set('name', 'Alex');
-		joe.save()
+	function assertName(operation, done){
+		operation
 			.then(() => User.find({}))
 			.then((users) => {
 				assert(users.length === 1);
 				assert(users[0].name === 'Alex');
 				done();
 			});
+	}
+
+	it('instance type using set n save', (done) => {
+		// set is instance method that allows you update a property (piecemeal update)
+		joe.set('name', 'Alex');
+		assertName(joe.save(), done);
+			
+	});
+
+	// Update() saves everything all at once
+	it('A model instance can update', (done) => {
+		assertName(joe.update({ name: 'Alex' }), done);
 	});
 });
