3158cd81aeddb2b0e4696f069d8c1cf0b7711eaf
Coffeeteer <tgrieshiemer@gmail.com>
2017-09-02 22:57:50 -0400

    working lecture 42, getting error: TypeError: Cannot read property 'errors' of undefined

--- test/update_test.js
+++ test/update_test.js
@@ -5,7 +5,7 @@ describe('Updating records', () => {
 	var joe;
 
 	beforeEach((done) => {
-		joe = new User({name: 'Joe'});
+		joe = new User({name: 'Joe', postCount: 0 });
 		joe.save()
 			.then(() => done());
 	});
@@ -30,5 +30,36 @@ describe('Updating records', () => {
 	// Update() saves everything all at once
 	it('A model instance can update', (done) => {
 		assertName(joe.update({ name: 'Alex' }), done);
+	});
+
+	it('a model class can update', (done) => {
+		assertName(
+			User.update({ name: 'Joe' }, { name: 'Alex' }),
+			done
+		);
+	});
+
+	it('a model class can update one record', (done) => {
+		assertName(
+			User.findOneAndUpdate({name: 'Joe'}, {name: 'Alex'}),
+			done
+		);
+	});
+
+	it('a model class can find a recored with an Id and update', (done) => {
+		assertName(
+			User.findByIdAndUpdate(joe._id, { name: 'Alex' }),
+			done
+		);
+	});
+
+	//Update Operators
+	it('A user can have their post count incremented by one', (done) => {
+		User.update({name: 'Joe'}, {$inc: {postCount: 10}})
+			.then(() => User.findOne({name: 'Joe'}))
+			.then((user) => {
+				assert(user.postCount === 10);
+				done();
+			})
 	});
 });
