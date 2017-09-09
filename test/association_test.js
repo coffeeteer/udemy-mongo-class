ae18994643ceee47a667f50c8c8812971ac81e7f
Coffeeteer <tgrieshiemer@gmail.com>
2017-09-08 22:57:58 -0400

    finished lecture 62

--- test/association_test.js
+++ test/association_test.js
@@ -22,12 +22,41 @@ describe('Assocations', () => {
 
 	it('saves a relation between a user and a blogpost', (done) => {
 		User.findOne({ name: 'Joe' })
-			// Population is the process of automatically replacing the specified paths in the document
+			// popluate() is the process of automatically replacing the specified paths in the document
 			// with documents from other collections. We may populate a single document, multiple 
 			//documents, plain objects, multiple plain objects, or all objects returned from a query.
+			// When the populate method is used, an array documents will be returned inplace of the 
+			// original _id
 			.populate('blogPosts')
 			.then((user) => {
 				assert(user.blogPosts[0].title === 'JS is great.');
+				done();
+			});
+	});
+
+	it('saves a full relation graph', (done) => {
+		User.findOne({ name: 'Joe'})
+			.populate({
+				path: 'blogPosts',
+				populate: {
+					path: 'comments',
+					model: 'comment',
+					populate: {
+						path: 'user',
+						model: 'user'
+					}
+				}
+			})
+			.then((user) => {
+				// console.log(user.blogPosts[0]);
+				// console.log(user.blogPosts[0].comments[0]);
+				assert(user.name === 'Joe');
+				assert(user.blogPosts[0].title === 'JS is great.');
+				assert(user.blogPosts[0].comments[0].content === 'Congrats in great post.' );
+				assert(user.blogPosts[0].comments[0].user.name === 'Joe');
+
+				// --- Personal CHALLENGE --- destructure these above properties.
+
 				done();
 			});
 	});
